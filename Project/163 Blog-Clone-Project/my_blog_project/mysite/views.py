d41fdf94fc9193d9c1ab863ea88c78631f7a36a6
nitinradke <nitinradkeirctc@gmail.com>
2018-03-30 12:23:32 +0530

    modified urls.py+views.py

--- Project/163 Blog-Clone-Project/my_blog_project/mysite/views.py
+++ Project/163 Blog-Clone-Project/my_blog_project/mysite/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from django.views.generic import ListView
+from mysite.models import Post,Comment
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
